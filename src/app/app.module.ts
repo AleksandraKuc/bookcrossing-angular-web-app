b4ba66c8b802b5a96a73ad226fb92d577f59bde8
Malawredna <olakuc17@wp.pl>
2020-11-02 22:25:51 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from "@angular/common/http";
+import { AboutBookcrossingComponent } from './about-bookcrossing/about-bookcrossing.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import {RouterModule} from "@angular/router";
+import {SharedModule} from "./shared.module";
+import { ContactComponent } from './contact/contact.component';
+import {RouterOutletModule} from "../../framework/app/router-outlet/router-outlet.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavigationComponent,
+    AboutBookcrossingComponent,
+    MainPageComponent,
+    ContactComponent,
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    SharedModule.forRoot(),
+    RouterOutletModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
